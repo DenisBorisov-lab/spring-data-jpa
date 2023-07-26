2346c87167fb85b9a680075bb1aa4c23842452f6
DenisBorisov-lab <mai.borisov.denis@gmail.com>
2023-07-27 00:33:04 +0300

    edited transactional

--- src/main/java/com/example/springdatajpa/service/PersonService.java
+++ src/main/java/com/example/springdatajpa/service/PersonService.java
@@ -2,12 +2,10 @@ package com.example.springdatajpa.service;
 
 import com.example.springdatajpa.model.Person;
 import com.example.springdatajpa.repository.PersonRepository;
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
@@ -17,15 +15,16 @@ public class PersonService {
     private PersonRepository personRepository;
 
     @Autowired
-    public PersonService(PersonRepository personRepository){
+    public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public void savePerson(Person person){
+    public void savePerson(Person person) {
         log.info("Поступил запрос на сохранение сущности");
         personRepository.save(person);
     }
-    public Person findByFirstName(String firstName){
+
+    public Person findByFirstName(String firstName) {
         Person person = personRepository.findByFirstName(firstName);
         return person;
     }
